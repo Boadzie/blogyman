bc9121bbd734495b3f7636a74ac8a3b273536f4a
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-10-01 08:36:31 +0000

    Create a blog app using Django
    Add user authentication to app
    
    Add Tailwind css to your app

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import (
+    BlogCreateView,
+    BlogDeleteView,
+    BlogDetailView,
+    BlogListView,
+    BlogUpdateView,
+)
+
+urlpatterns = [
+    path("", BlogListView.as_view(), name="home"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("posts/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("posts/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
+]
---
+++ blog/views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    queryset = Post.objects.all().order_by("-id")
+    context_object_name = "post_list"
+    paginate_by = 3
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = "title", "body"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
