c89b7d6ca8f66d8be93db7ac0b249764e8e2ba57
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-10-02 16:01:56 +0000

    Add support for environment variables
    Install python dotenv package

--- config/settings.py
+++ config/settings.py
@@ -1,15 +1,14 @@
 import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "secret44a4a@@a")
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
