122a6a954fa162db7f0d486f3ff4d245d3be7aae
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-10-02 12:22:35 +0000

    Add allowed origins to settings

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "secret44a4a@@a")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
